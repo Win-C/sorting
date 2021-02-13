050afcd401f07bf08ad1f7a4f8fd01b560d0104c
Winnie Chou <winnie.chou.mb@gmail.com>
2021-02-12 18:50:25 -0600

    completed merge and mergeSort methods

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+"use strict";
+
+/** Function sorts the values in the array using the BubbleSort algorithm
+ *  Takes in an array
+ *  Returns mutated, sorted array
+ *  O(n^2)
+ */
+
+function bubbleSort(array) {
+  for (let i=0; i<array.length; i++){
+    for (let j=i+1; j<array.length; j++){
+      if (array[i] > array[j]) {
+        let temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+
+  return array;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,61 @@
-function merge() {}
+"use strict";
 
-function mergeSort() {}
+/** Function accepts two `sorted arrays
+ *  Returns a new array with values from both arrays sorted
+ *  
+ *  O(n + m) time and O(n + m) space
+ */
 
-module.exports = { merge, mergeSort};
+function merge(arr1, arr2) {
+  let mergedArr = [];
+  let pointer1 = 0; // pointer for arr1
+  let pointer2 = 0; // pointer for arr2
+
+  while (pointer1 < arr1.length && pointer2 < arr2.length) {
+    if (arr1[pointer1] < arr2[pointer2]) {
+      mergedArr.push(arr1[pointer1]);
+      pointer1++;
+    } else {
+      mergedArr.push(arr2[pointer2]);
+      pointer2++;
+    }
+  }
+
+  while (pointer1 < arr1.length) {
+    mergedArr.push(arr1[pointer1]);
+    pointer1++;
+  }
+
+  while (pointer2 < arr2.length) {
+    mergedArr.push(arr2[pointer2]);
+    pointer2++;
+  }
+
+  return mergedArr;
+}
+
+
+/** Given an array, function will sort the values in the array using the
+ *  merge sort algorithm:
+ *    - breaks up the array into smaller arrays: either empty or only 1 element
+ *    - builds up a newly sorted array from the smaller arrays
+ * 
+ *  O(nlogn)
+*/
+
+function mergeSort(array) {
+  // base case is when array is length of 0 or 1
+  if (array.length <= 1) return array;
+
+  // recursively break down array into smaller arrays
+  let midpoint = Math.floor(array.length / 2);
+  let half1 = array.slice(0, midpoint);
+  let half2 = array.slice(midpoint);
+  let arr1 = mergeSort(half1);
+  let arr2 = mergeSort(half2);
+
+  let sortedArr = merge(arr1, arr2);
+  return sortedArr;
+}
+
+module.exports = { merge, mergeSort };
